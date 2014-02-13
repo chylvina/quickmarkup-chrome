1c2ed6533539d6e75ea9a6a4657447a9fdbc8764
chylvina <chylvina@gmail.com>
2014-02-13 12:02:12 +0800

    clean

--- javascripts/show.js
+++ javascripts/show.js
@@ -3,10 +3,6 @@ configEventListener('doMarkup');
 configEventListener('doUpload');
 configEventListener('doSave');
 configEventListener('doCopy');
-//configEventListener('doSinaWB');
-
-//document.getElementById('btn_logon_sinawb').addEventListener('click', toDo, false);
-//document.getElementById('btn_send').addEventListener('click', toDo, false);
 
 function configEventListener(objID) {
 	document.getElementById(objID).addEventListener('click', toDo, false);
@@ -100,10 +96,8 @@ function onUploadComplete(url) {
 	imageURL = url
 	i18nReplace("doUpload" , "uploaded");
 	$("doUpload").style.cursor = "pointer";
-	if(!publisher.enabled) {
-		resultPanelHandler.popup(chrome.i18n.getMessage("uploadComplete") + 
+		resultPanelHandler.popup(chrome.i18n.getMessage("uploadComplete") +
 			'<br/><a href="' + url + '" target="_blank">' + url + '</a>');
-	}
 }
 
 function onUploadFailed() {
@@ -226,20 +220,6 @@ function toDo(evt) {
 				i18nReplace("doCopy" , "copyFailed");
 			}
 			break;
-		case 'doSinaWB':
-			publisher.init();
-			break;
-		case 'btn_logon_sinawb':
-			SinaMicroblog.getRequestToken();
-			break;
-		case 'btn_send':
-			publisher.doUpload();
-			break;
-		case 'changeAccount':
-			SinaMicroblog.logout(function() {
-				  publisher.showErrorInfo("");
-				});
-			break;
 	}
 }
 
@@ -273,10 +253,7 @@ chrome.extension.onRequest.addListener(function(request, sender, response) {
 
 var resultPanelHandler = {
 	popup: function(s){
-		if(publisher.enabled) {
-			return;
-		}
-		
+
 		s = s || '';
 		// init display
 		$("resultPanel").style.display = "block";
@@ -296,205 +273,5 @@ var resultPanelHandler = {
 		//
 		$("panelBG").removeEventListener("click" , resultPanelHandler.close);
 		$("closeResultPanel").removeEventListener("click" , resultPanelHandler.close);
-	}
-}
-var publisher = {
-	firstTime: true,
-	enabled: false,
-	init: function() {
-		if(publisher.enabled) {
-			return;
-		}
-		publisher.enabled = true;
-		
-		// init display
-		$("sharePanel").style.display = "block";
-		$("panelBG").style.display = "block";
-		$("sharePanel").style.left = (document.body.clientWidth - $("sharePanel").clientWidth) / 2 + "px";
-		$("sharePanel").style.top = (document.body.clientHeight - $("sharePanel").clientHeight) / 2 + "px";
-		
-		//
-		if(publisher.firstTime) {
-			publisher.firstTime = false;
-			var user = Account.getUser(SinaMicroblog.siteId);
-			if(user) {
-				publisher.onAccountReady();
-			} else {
-				publisher.showErrorInfo('');
-			}
-		}
-		
-		// config event
-		$("txt").addEventListener("focus" , publisher.limitLength);
-		$("txt").addEventListener("blur" , publisher.limitLength);
-		$("txt").addEventListener("keyup" , publisher.limitLength);
-		$("panelBG").addEventListener("click" , publisher.close);
-		$("closeSharePanel").addEventListener("click" , publisher.close);
-	},
-	
-	close: function() {
-		if(!publisher.enabled) {
-			return;
-		}
-		publisher.enabled = false;
-		
-		$("sharePanel").style.display = "none";
-		$("panelBG").style.display = "none";
-		
-		//
-		txt.removeEventListener("focus" , publisher.limitLength);
-		txt.removeEventListener("blur" , publisher.limitLength);
-		txt.removeEventListener("keyup" , publisher.limitLength);
-		$("panelBG").removeEventListener("click" , publisher.close);
-		$("closeSharePanel").removeEventListener("click" , publisher.close);
-	},
-	
-	onAccountReady: function(name) {
-		$("btn_logon_sinawb").style.display = "none";
-		$("btn_send").style.display = "block";
-		$("accountInfo").style.display = "block";
-		$("outputInfo").style.display = "none";
-		$("userName").innerHTML = Account.getUser(SinaMicroblog.siteId)['name'];
-		
-		publisher.enablePublishBtn();
-	},
-	
-	doUpload: function() {
-		if(Account.getUser(SinaMicroblog.siteId)) {
-			var access_token = Account.getUser(SinaMicroblog.siteId)['accessToken'];
-			var successCallback;
-			var failureCallback;
-			
-			if (access_token) {
-				var userId = Account.getUser(SinaMicroblog.siteId)['id'];
-				var access_token_secret = Account.getUser(SinaMicroblog.siteId)['accessTokenSecret'];
-				successCallback = function(data) {
-					publisher.close();
-					var url = 'http://www.weibo.com/' + userId;
-					resultPanelHandler.popup('发布微博成功 :)<br /><a href="' + url + '" target="_blank">访问我的微博</a>');
-					$("txt").disabled = false;
-					publisher.enablePublishBtn();
-					$("loadingIcon").style.display = "none";
-				};
-				failureCallback = function(errorData) {
-					publisher.showErrorInfo("上传失败<br />请重新登录");
-					$("txt").disabled = false;
-					publisher.enablePublishBtn();
-					$("loadingIcon").style.display = "none";
-				};
-				SinaMicroblog.upload(access_token, access_token_secret, $("txt").value,
-						successCallback, null, failureCallback);
-				$("loadingIcon").style.display = "block";
-				publisher.disablePublishBtn();
-				$("txt").disabled = true;
-			} else {
-				//SinaMicroblog.getAccessToken();
-			}
-		}
-	},
-	
-	showErrorInfo: function(s) {
-		$("btn_logon_sinawb").style.display = "block";
-		$("btn_send").style.display = "none";
-		$("accountInfo").style.display = "none";
-		$("outputInfo").style.display = "table-cell";
-		$("outputInfo").innerHTML = s;
-		Account.removeUser(SinaMicroblog.siteId);
-	},
-	
-	getPhotoData: function() {
-		var dataUrl = $('image').src;
-		var photoDataIndex = dataUrl.indexOf('data:image/png;base64,');
-		if (photoDataIndex != 0) {
-		  return;
-		}
-	
-		// Decode to binary data
-		return atob(dataUrl.substr(photoDataIndex + 22));
-	},
-	
-	// ------------ String Util -----------------
-	limitLength: function() {
-		var sLength = 140;
-		var snapLength = publisher.getLength($("txt").value);
-		if (snapLength > sLength) {
-			$("txt_count_msg").innerHTML = "已超出<em>" + (snapLength - sLength) + "</em>字";
-		} else {
-			$("txt_count_msg").innerHTML = "还可以输入<em>" + (sLength - snapLength) + "</em>字";
-		}
-		$("txt_count_msg").className = snapLength == 0 || snapLength <= sLength ? "" : "red";
-		publisher.allow(snapLength <= sLength && snapLength > 0);
-	},
-	
-	allow: function (b) {
-		if (!b) {
-			publisher.disablePublishBtn();
-		} else {
-			publisher.enablePublishBtn();
-		}
-	},
-	
-	enablePublishBtn: function() {
-        document.getElementById('btn_send').addEventListener('click', toDo, false);
-		$("btn_send").style.backgroundPosition = "";
-		$("btn_send").getElementsByTagName("span")[0].style.backgroundPosition = "";
-		$("btn_send").getElementsByTagName("span")[0].style.color = "#fff";
-		$("btn_send").style.cursor = "pointer";
-	},
-	
-	disablePublishBtn: function() {
-        document.getElementById('btn_send').removeEventListener('click', toDo, false);
-		$("btn_send").style.backgroundPosition = "100% -30px";
-		$("btn_send").getElementsByTagName("span")[0].style.backgroundPosition = "0% -30px";
-		$("btn_send").getElementsByTagName("span")[0].style.color = "#eee";
-		$("btn_send").style.cursor = "default";
-	},
-	
-	getLength: function (str) {
-		var len = publisher.trim(str).length;
-		if (len > 0) {
-			var min = 41,
-				max = 140,
-				surl = 20,
-				tmp = str;
-			var urls = str.match(/http:\/\/[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+([-A-Z0-9a-z\$\.\+\!\*\(\)//,:;@&=\?\~\#\%]*)*/gi) || [];
-			var urlCount = 0;
-			for (var i = 0, len = urls.length; i < len; i++) {
-				var count = publisher.byteLength(urls[i]);
-				if (/^(http:\/\/t.cn)/.test(urls[i])) {
-					continue;
-				} else {
-					if (/^(http:\/\/)+(t.sina.com.cn|t.sina.cn)/.test(urls[i])) {
-						urlCount += count <= min ? count : (count <= max ? surl : (count - max + surl));
-					} else {
-						urlCount += count <= max ? surl : (count - max + surl);
-					}
-				}
-				tmp = tmp.replace(urls[i], "");
-			}
-			return Math.ceil((urlCount + publisher.byteLength(tmp)) / 2);
-		} else {
-			return 0;
-		}
-	},
-	
-	trimHead: function (str) {
-		return str.replace(/^(\u3000|\s|\t)*/gi, "");
-	},
-	
-	trimTail: function (str) {
-		return str.replace(/(\u3000|\s|\t)*$/gi, "");
-	},
-	
-	trim: function (str) {
-		return publisher.trimHead(publisher.trimTail(str));
-	},
-	
-	byteLength: function (str) {
-		if (typeof str == "undefined") {
-			return 0;
-		}
-		var aMatch = str.match(/[^\x00-\x80]/g);
-		return (str.length + (!aMatch ? 0 : aMatch.length));
 	}
 }
